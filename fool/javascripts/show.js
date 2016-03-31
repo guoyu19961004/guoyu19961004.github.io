e4591feb46f5f8a7954c19e89a38d50574344a61
郭宇 <u201413384@hust.edu.cn>
2016-03-31 08:39:11 +0800

    debug

--- fool/javascripts/show.js
+++ fool/javascripts/show.js
@@ -40,7 +40,7 @@ function judgeUser() {
 	if (getQueryString("jump")) {
 		share.style.display = "block";
 		create.style.display = "none";
-		window.history.replaceState(null, null, "?sex=" + getQueryString("sex") + "&name=" + getQueryString("name"));
+		window.history.pushState(null, null, "?sex=" + getQueryString("sex") + "&name=" + getQueryString("name"));
 	}
 	judgeSex();
 	judgeName();
